706a44e992b34a1871b6232e910010f5fef59e3f
rafion <rafion.torres@gmail.com>
2021-12-02 20:41:15 -0300

    register form

--- src/app/routes/sessions/register/register.component.ts
+++ src/app/routes/sessions/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [this.confirmValidator]],
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  confirmValidator = (control: FormControl): { [k: string]: boolean } => {
+    if (!control.value) {
+      return { error: true, required: true };
+    } else if (control.value !== this.registerForm.controls.password.value) {
+      return { error: true, confirm: true };
+    }
+    return {};
+  };
 
 }
